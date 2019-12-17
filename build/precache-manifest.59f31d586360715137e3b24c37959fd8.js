7de0a7a5612df6afcc5f41c95732203b617acc13
StringTek <stringtek1998@outlook.com>
2019-12-17 21:16:38 +0800

    add build folder

---
+++ build/precache-manifest.59f31d586360715137e3b24c37959fd8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c41bfd2ff7f0aac41c5526d14b53116f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a35043b026d68e7bd6c",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6a55daebb41d726a7dbc",
+    "url": "/static/js/2.0f1ec884.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0f1ec884.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4a35043b026d68e7bd6c",
+    "url": "/static/js/main.941f2d2d.chunk.js"
+  },
+  {
+    "revision": "a2b37c230b887581cfb1",
+    "url": "/static/js/runtime-main.2bb234aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
